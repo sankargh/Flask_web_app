743b43bbadac794a858654cf0dc609c72156b5f5
sankargh <99525703+sankargh@users.noreply.github.com>
2026-04-01 13:15:25 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    # You can run your python logic here and pass data to the template
+    script_output = "Hello from the Flask server!"
+    return render_template('index.html', output=script_output)
+
+if __name__ == '__main__':
+    app.run(debug=True)
