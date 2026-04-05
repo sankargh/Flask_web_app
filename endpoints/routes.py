340f9a00af8bd04783543050469eb8a237e67a1e
sankargh <99525703+sankargh@users.noreply.github.com>
2026-04-05 22:57:53 +0800

    Replace agent.say_hello with local say_hello function

--- endpoints/routes.py
+++ endpoints/routes.py
@@ -1,12 +1,14 @@
 from flask import Blueprint, jsonify
-from .. import agent
+# from .. import agent
 
 api_bp = Blueprint("api", __name__)
 
-
+def say_hello():
+    return "Hello from same method!"
+    
 @api_bp.get("/hello")
 def hello_world():
-    message = agent.say_hello()
+    message = say_hello()
     return message
 
 @api_bp.get("/api/data")
