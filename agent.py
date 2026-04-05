b545f56b0374e54cd0f6f719eacb41049518c397
sankargh <99525703+sankargh@users.noreply.github.com>
2026-04-05 20:24:58 +0800

    Add about_maxwell function and Gradio interface
    Added a new function 'about_maxwell' to provide information about the Maxwell MRT station in Singapore. Integrated Gradio for chat interface and loaded environment variables.

--- agent.py
+++ agent.py
@@ -1,4 +1,29 @@
-# Function definition
+import gradio as gr
+from agents import Agent, Runner, function_tool, trace
+from dotenv import load_dotenv
+
+load_dotenv(override=True)
+
+# Hello Function definition
 def say_hello():
     """Returns simple hello text."""
     return "Hello, World!"
+
+# Maxwell Function definition
+@function_tool
+def about_maxwell():
+    """ Gets detail about maxwell """
+    return "Maxwell is an MRT in Singapore. It is part of 'Thompson East coast line'. The station is closer to China town"
+    
+agent = Agent(
+    name="Test Agent",
+    instructions="Tell me about in Singapore with tools provided",
+    tools=[about_maxwell],
+    model="gpt-4o-mini"
+)
+
+async def chat(user_input: str, history):
+    with trace("Test API from Git"):
+        result = await Runner.run(agent,user_input)
+    return result.final_output
+gr.ChatInterface(fn=chat).launch()
